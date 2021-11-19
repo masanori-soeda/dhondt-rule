9be4877ef34dced7cf81a8d3fe4f1b9bf8a46306
masanori soeda <m.soepy@gmail.com>
2021-11-19 18:28:32 +0900

    init

---
+++ src/main/java/dhondtrule/dhondt/DhondtApplication.java
@@ -0,0 +1,13 @@
+package dhondtrule.dhondt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhondtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhondtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dhondtrule/dhondt/DhondtApplicationTests.java
@@ -0,0 +1,13 @@
+package dhondtrule.dhondt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhondtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
